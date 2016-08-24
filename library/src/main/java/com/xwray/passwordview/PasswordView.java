548faba12ef55c7b5a81b6d6354fbfaa5b9dc425
Pawel Hajduk <pawel.hajduk89@gmail.com>
2016-08-24 13:06:30 +0200

    Eye is hidden when no text available

--- library/src/main/java/com/xwray/passwordview/PasswordView.java
+++ library/src/main/java/com/xwray/passwordview/PasswordView.java
@@ -24,13 +24,13 @@ import android.widget.EditText;
  */
 public class PasswordView extends EditText {
 
-    private Drawable eye;
-    private Drawable eyeWithStrike;
     private final static int VISIBILITY_ENABLED = (int) (255 * .54f); // 54%
     private final static int VISIBLITY_DISABLED = (int) (255 * .38f); // 38%
+    private Drawable eye;
+    private Drawable eyeWithStrike;
     private boolean visible = false;
     private boolean useStrikeThrough = false;
-    private int eyeTint = Color.BLACK;
+    private int     eyeTint = Color.BLACK;
     private Typeface typeface;
 
     public PasswordView(Context context) {
@@ -48,17 +48,57 @@ public class PasswordView extends EditText {
         init(attrs);
     }
 
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP) public PasswordView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public PasswordView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
         init(attrs);
     }
 
+    @Override
+    public void setInputType(int type) {
+        this.typeface = getTypeface();
+        super.setInputType(type);
+        setTypeface(typeface);
+    }
+
+    @Override
+    protected void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter) {
+        super.onTextChanged(text, start, lengthBefore, lengthAfter);
+        if (this.getText().toString().length() > 0) {
+            setDrawable();
+        } else {
+            removeDrawable();
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (getCompoundDrawables()[2] != null && event.getAction() == MotionEvent.ACTION_UP
+                && event.getX() >= (getRight() - getCompoundDrawables()[2].getBounds().width())) {
+            visible = !visible;
+            setup();
+            invalidate();
+            return true;
+        }
+
+        return super.onTouchEvent(event);
+    }
+
+    public void setEyeTint(@ColorInt int eyeTint) {
+        this.eyeTint = eyeTint;
+    }
+
+    public void setUseStrikeThrough(boolean useStrikeThrough) {
+        this.useStrikeThrough = useStrikeThrough;
+    }
+
     private void init(AttributeSet attrs) {
         if (attrs != null) {
-            TypedArray a = getContext().getTheme().obtainStyledAttributes(
-                    attrs,
-                    R.styleable.PasswordView,
-                    0, 0);
+            TypedArray a = getContext().getTheme()
+                                       .obtainStyledAttributes(attrs,
+                                                               R.styleable.PasswordView,
+                                                               0,
+                                                               0);
             try {
                 useStrikeThrough = a.getBoolean(R.styleable.PasswordView_useStrikeThrough, false);
                 eyeTint = a.getColor(R.styleable.PasswordView_eyeTint, Color.BLACK);
@@ -75,39 +115,25 @@ public class PasswordView extends EditText {
         DrawableCompat.setTint(eyeWithStrike, eyeTint);
         eyeWithStrike.setAlpha(VISIBILITY_ENABLED);
         setup();
+        removeDrawable();
     }
 
     protected void setup() {
-        setInputType(InputType.TYPE_CLASS_TEXT | (visible ? InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD : InputType.TYPE_TEXT_VARIATION_PASSWORD));
-        Drawable drawable = useStrikeThrough && !visible ? eyeWithStrike : eye;
-        Drawable[] drawables = getCompoundDrawables();
-        setCompoundDrawablesWithIntrinsicBounds(drawables[0], drawables[1], drawable, drawables[3]);
+        setInputType(InputType.TYPE_CLASS_TEXT
+                             | (visible ? InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD :
+                InputType.TYPE_TEXT_VARIATION_PASSWORD));
+
+        setDrawable();
         eye.setAlpha(visible && !useStrikeThrough ? VISIBILITY_ENABLED : VISIBLITY_DISABLED);
     }
 
-    @Override public boolean onTouchEvent(MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_UP
-                && event.getX() >= (getRight() - getCompoundDrawables()[2].getBounds().width())) {
-            visible = !visible;
-            setup();
-            invalidate();
-            return true;
-        }
-
-        return super.onTouchEvent(event);
+    private void setDrawable() {
+        Drawable drawable = useStrikeThrough && !visible ? eyeWithStrike : eye;
+        Drawable[] drawables = getCompoundDrawables();
+        setCompoundDrawablesWithIntrinsicBounds(drawables[0], drawables[1], drawable, drawables[3]);
     }
 
-    @Override public void setInputType(int type) {
-        this.typeface = getTypeface();
-        super.setInputType(type);
-        setTypeface(typeface);
-    }
-
-    public void setEyeTint(@ColorInt int eyeTint) {
-        this.eyeTint = eyeTint;
-    }
-
-    public void setUseStrikeThrough(boolean useStrikeThrough) {
-        this.useStrikeThrough = useStrikeThrough;
+    private void removeDrawable() {
+        this.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
     }
 }
